42adc5b5ad6f18cdff7326af81544652ff9571bd
watchdog92 <cristhian.pt.1992@gmail.com>
2023-09-22 15:16:24 -0500

    select modificado en productos

--- selecciones/select productos script.sql
+++ selecciones/select productos script.sql
@@ -13,6 +13,6 @@ values(7,'gomitas',4.00,100)
 insert into productos (codigo,nombre,precio,stock)
 values(8,'azucar',5.50,50)
 select *from productos
-select *from productos where nombre='Q'
+select *from productos where nombre like'Q%'
 select *from productos where descripcion is null
 select *from productos where precio between money(2) and money(3) 
