42efa33e0175d080356e37b2649a97ed95f3dd3b
watchdog92 <cristhian.pt.1992@gmail.com>
2023-09-23 17:06:17 -0500

    eliminaciones realizadas

---
+++ eliminacion/eliminacion estudiantes.sql
@@ -0,0 +1,31 @@
+create table estudiantes(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key (cedula)
+)
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('1234567890' ,'Ana','Rodríguez','ana.rodriguez@example.com','15/05/1990')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('2345678901' ,'Juan','López','juan.lopez@egmail.com','10/03/1991')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('3456789012' ,'María','Fernández','maria.fernandez@hotmail.com','02/12/1994')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('4567890123' ,'Carlos','Martínez','carlos.martinez@gmail.com','17/09/2005')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('5678901234' ,'Laura','Sanchez','laura.sanchez@outlook.com','12/05/1978')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('6789012345' ,'Javier','Felix','javier.felix@gmail.com','10/01/1992')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('7890123456' ,'Luis','Garcia','luis.garcia@gmail.com','10/09/1993')
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values('8901234567' ,'Isabel','Lopez','isabel.lopez@hotmail.com','15/05/1999')
+select *from estudiantes
+select nombre, cedula from estudiantes
+select nombre from estudiantes where cedula like '17%'
+select nombre,apellido from estudiantes where nombre like 'A%'
+update estudiantes set apellido='Hernandez'
+where cedula like '17%'
+delete from estudiantes where cedula like '%05'
---
+++ eliminacion/eliminacion transferencias.sql
@@ -0,0 +1,40 @@
+create table transacciones(
+	codigo int not null,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hora time not null,
+	constraint transacciones_pk primary key (codigo)
+)
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(43,'56789',9.80,'C','03/12/2000','11:45')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(55,'58390',99.70,'C','06/11/2020','10:32')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(32,'95040',109.00,'C','13/09/2021','01:05')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(78,'27923',27.40,'D','14/07/2022','11:02')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(87,'59303',10383.30,'D','08/12/2019','09:42')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(90,'04995',4322.30,'C','23/11/2017','12:30')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(99,'18383',34.80,'D','20/10/2023','07:00')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(39,'32828',0.20,'C','11/12/2020','04:05')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(66,'83483',1.20,'D','01/11/2016','03:55')
+insert into transacciones (codigo,numero_cuenta,monto,tipo,fecha,hora)
+values(33,'38583',8.77,'D','02/06/2010','07:25')
+select *from transacciones
+select *from transacciones where tipo='D'
+select *from transacciones where monto between money(200) and money(2000)
+select codigo, monto, tipo,fecha from transacciones where fecha is not null
+
+update transacciones set tipo='T'
+where monto>money(100) and monto<money(500)
+AND fecha between '01/09/1900' and '30/09/2050' 
+AND hora between '14:00' and '20:00'
+
+delete from transacciones where hora between '14:00:00' and '18:00:00' AND fecha between '01/08/2023' and '31/08/2023'
